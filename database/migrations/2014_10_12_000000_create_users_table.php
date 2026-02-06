858b5927f64602725e3c90417bb83d380555882a
Rifqi <colabfre2@gmail.com>
2026-02-06 11:07:06 +0700

    add

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->enum('role', ['admin', 'user', 'guest'])->default('user');
+            $table->enum('role', ['admin', 'member', 'guest'])->default('guest');
             $table->enum('gender', ['male', 'female'])->default('male');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,5 +15,22 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('users')->insert(
+            ([
+            'name' => 'Broto suciptop',
+            'email' => 'broto@admin.com',
+            'password' => Hash::make('broto123'),
+            'role' => 'admin',
+            'gender' => 'male'
+        ]),
+        ([
+            'name' => 'budiman suciptop',
+            'email' => 'budiman@member.com',
+            'password' => Hash::make('budiman123'),
+            'role' => 'member',
+            'gender' => 'male'
+        ]),
+        );
+
     }
 }
