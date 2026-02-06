cd935a01ff6e1b04e8e3d5ac8d73dd17dab1e80a
LegendDK <muhammadeizza@gmail.com>
2026-02-06 09:26:50 +0700

    #

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\support\Facades\Hash;
+use Illuminate\support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    function index(){
+        return view('login.index');
+    }
+
+    function login(Request $request){
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $inputLogin = $request->only('email', 'password');
+        $user = User::where('email', $inputLogin['email'])->first();
+        if($user && Hash::check($inputLogin['password'], $user->password)){
+            Auth::login($user);
+
+            if($user->role == 'admin'){
+                return redirect('/admin');
+            }
+        }
+    }
+}
