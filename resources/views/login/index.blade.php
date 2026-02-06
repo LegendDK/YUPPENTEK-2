7c0117cf22057a35707b0866b7e2e6511ed0d096
LegendDK <muhammadeizza@gmail.com>
2026-02-06 09:58:35 +0700

    Add View

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -12,8 +12,19 @@
             <h1>Selamat Datang</h1>
             <p>Login Untuk Melanjutkan</p>
         </div>
-
-        
+        @error('login')
+            <p style="color: red">{{ $message }}</p>
+        @enderror
+        <form action="{{ Route('login') }}" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="email">Email : </label>
+                <input type="email" name="email" id="email" class="form-input" value="{{ old('email') }}">
+                @error('email')
+                    <p style="color: red">{{ $message }}</p>
+                @enderror
+            </div>
+        </form>
     </div>
 </body>
 </html>
