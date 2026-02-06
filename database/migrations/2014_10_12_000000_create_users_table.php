a32e8e13fd59f57cac3b3858b4c345e56aab3c0b
Rifqi <colabfre2@gmail.com>
2026-02-06 10:14:36 +0700

    add migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['admin', 'user', 'guest'])->default('user');
+            $table->enum('gender', ['male', 'female'])->default('male');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
